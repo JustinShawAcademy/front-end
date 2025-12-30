1c54432d30a9c40576a76754d8a54f4e8be54213
Justin Shaw <justinshawcareer@gmail.com>
2025-12-30 18:38:31 -0500

    comp: 2-rock-paper-scissors backend game

---
+++ projects/2-rock-paper-scissors/javascript.js
@@ -0,0 +1,48 @@
+const choice = ['rock', 'paper', 'scissors'];
+
+let humanScore = 0;
+let computerScore = 0;
+
+const winAgainst = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+}
+
+    
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    return choice[randomNum];
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Select [rock, paper, scissors]: ");
+
+    if (humanChoice == null) {
+        console.log("Enter a valid input!");
+    }
+    return humanChoice.trim().toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    let outcome = "";
+    if (humanChoice == computerChoice) {
+        outcome = `Tie!: Both Human and Computer selected ${humanChoice}`;
+    }
+    else if (winAgainst[humanChoice] == computerChoice) {
+        outcome = `Win!: Human's ${humanChoice} beats Computer's ${computerChoice}`;
+        humanScore++;
+    }
+    else {
+        outcome = `Lose!: Human's ${humanChoice} loses againt Computer's ${computerChoice}`;
+        computerScore++;
+    }
+    console.log(outcome);
+    console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+}
+
+function playGame(numOfRounds) {
+    for (let i = 0; i < numOfRounds; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+}
