b52d5a9834c695d2e7c9daf9b462d7e3cf457e98
Justin Shaw <justinshawcareer@gmail.com>
2026-03-21 15:01:04 -0400

    refactor: Change SearchBar to be SearchHeader
    This is since SearchBar.jsx includes the settings buttontoo.

--- projects/000-weather-app/src/App.jsx
+++ projects/000-weather-app/src/App.jsx
@@ -1,10 +1,10 @@
-import SearchBar from "./components/SearchBar"
+import SearchHeader from "./components/SearchHeader"
 
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-blue-50 p-4">
       <div className="w-full max-w-5xl rounded-3xl bg-white p-8 shadow-sm">
-        <SearchBar />
+        <SearchHeader />
       </div>
     </div>
   )
